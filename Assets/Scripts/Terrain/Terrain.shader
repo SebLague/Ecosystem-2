076fff626c23fd19ea707752a3ac87bad0f26f18
Sebastian Lague <sebastlague@gmail.com>
2019-07-11 22:43:36 +0200

    Initial commit

---
+++ Assets/Scripts/Terrain/Terrain.shader
@@ -0,0 +1,57 @@
+﻿Shader "Custom/Terrain"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+        float4 _StartCols[3];
+        float4 _EndCols[3];
+        float4 test;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            int colIndex = (int)IN.uv_MainTex.x;
+            float t = IN.uv_MainTex.y;
+            o.Albedo = lerp(_StartCols[colIndex],_EndCols[colIndex],t);
+           // o.Albedo = t;
+            //o.Albedo = float4(1,0,1,1);
+            // Metallic and smoothness come from slider variables
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TerrainGenerator : MonoBehaviour {
+
+    const string meshHolderName = "Terrain Mesh";
+
+    public float waterDepth = .2f;
+    public int worldSize = 20;
+    [Range (0.2f, 2)]
+    public float cellSize = 1;
+
+    public NoiseSettings terrainNoise;
+    public Material mat;
+    public Material sideMat;
+
+    public Biome water;
+    public Biome sand;
+    public Biome grass;
+
+    [Header ("Info")]
+    public int numLandTiles;
+    public int numWaterTiles;
+
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+    Mesh mesh;
+
+    bool needsUpdate;
+
+    void Update () {
+        if (needsUpdate) {
+            needsUpdate = false;
+            Generate ();
+        }
+    }
+
+    public void Generate () {
+        CreateMeshComponents ();
+        numLandTiles = 0;
+        numWaterTiles = 0;
+
+        var biomes = new Biome[] { water, sand, grass };
+
+        int numCellsPerLine = Mathf.CeilToInt (worldSize / cellSize);
+        float actualWorldSize = cellSize * numCellsPerLine;
+        float min = -actualWorldSize / 2;
+        float[, ] map = HeightmapGenerator.GenerateHeightmap (terrainNoise, numCellsPerLine);
+
+        var vertices = new List<Vector3> ();
+        var triangles = new List<int> ();
+        var uvs = new List<Vector2> ();
+        var normals = new List<Vector3> ();
+
+        var sideTriangles = new List<int> ();
+
+        // 
+        Vector3[] normalsSet = { Vector3.up, Vector3.up, Vector3.up, Vector3.up };
+        Vector2Int[] nswe = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+        // Tile verts are in order: NW, NE, SW, SE
+        int[][] vertIndexByDir = { new int[] { 3, 2 }, new int[] { 0, 1 }, new int[] { 2, 0 }, new int[] { 1, 3 } };
+        Vector3[] sideNormalsByDir = { Vector3.forward, Vector3.back, Vector3.right, Vector3.left };
+
+        for (int y = 0; y < numCellsPerLine; y++) {
+            for (int x = 0; x < numCellsPerLine; x++) {
+                // Calculate biome
+                int biomeIndex = 0;
+                float biomeStartHeight = 0;
+                for (int i = 0; i < biomes.Length; i++) {
+                    if (map[x, y] <= biomes[i].height) {
+                        biomeIndex = i;
+                        break;
+                    }
+                    biomeStartHeight = biomes[i].height;
+                }
+
+                Biome biome = biomes[biomeIndex];
+                float sampleT = Mathf.InverseLerp (biomeStartHeight, biome.height, map[x, y]);
+                sampleT = (int) (sampleT * biome.numSteps) / (float) Mathf.Max (biome.numSteps, 1);
+
+                Vector2 uv = new Vector2 (biomeIndex, sampleT);
+                uvs.Add (uv);
+                uvs.Add (uv);
+                uvs.Add (uv);
+                uvs.Add (uv);
+
+                bool isWaterTile = biomeIndex == 0;
+                if (isWaterTile) {
+                    numWaterTiles++;
+                } else {
+                    numLandTiles++;
+                }
+
+                // Vertices
+                int vertIndex = vertices.Count;
+                float height = (isWaterTile) ? -waterDepth : 0;
+                Vector3 nw = new Vector3 (min + cellSize * x, height, -min - cellSize * y);
+                Vector3 ne = nw + Vector3.right * cellSize;
+                Vector3 sw = nw - Vector3.forward * cellSize;
+                Vector3 se = sw + Vector3.right * cellSize;
+                Vector3[] tileVertices = { nw, ne, sw, se };
+
+                // Add vertices
+                vertices.AddRange (tileVertices);
+                normals.AddRange (normalsSet);
+
+                // Add triangles
+                triangles.Add (vertIndex);
+                triangles.Add (vertIndex + 1);
+                triangles.Add (vertIndex + 2);
+                triangles.Add (vertIndex + 1);
+                triangles.Add (vertIndex + 3);
+                triangles.Add (vertIndex + 2);
+
+                if (isWaterTile) {
+                    for (int i = 0; i < nswe.Length; i++) {
+                        int neighbourX = x + nswe[i].x;
+                        int neighbourY = y + nswe[i].y;
+                        if (neighbourX >= 0 && neighbourX < numCellsPerLine && neighbourY >= 0 && neighbourY < numCellsPerLine) {
+                            float neighbourHeight = map[neighbourX, neighbourY];
+                            bool neighbourIsLand = neighbourHeight > biomes[0].height;
+                            if (neighbourIsLand) {
+                                vertIndex = vertices.Count;
+                                int edgeVertIndexA = vertIndexByDir[i][0];
+                                int edgeVertIndexB = vertIndexByDir[i][1];
+                                vertices.Add (tileVertices[edgeVertIndexA]);
+                                vertices.Add (tileVertices[edgeVertIndexA] + Vector3.up * waterDepth);
+                                vertices.Add (tileVertices[edgeVertIndexB]);
+                                vertices.Add (tileVertices[edgeVertIndexB] + Vector3.up * waterDepth);
+                                uvs.AddRange (new Vector2[] { Vector2.zero, Vector2.zero, Vector2.zero, Vector2.zero });
+                                sideTriangles.Add (vertIndex);
+                                sideTriangles.Add (vertIndex + 1);
+                                sideTriangles.Add (vertIndex + 2);
+                                sideTriangles.Add (vertIndex + 1);
+                                sideTriangles.Add (vertIndex + 3);
+                                sideTriangles.Add (vertIndex + 2);
+                                normals.AddRange (new Vector3[] { sideNormalsByDir[i], sideNormalsByDir[i], sideNormalsByDir[i], sideNormalsByDir[i] });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        mesh.subMeshCount = 2;
+        mesh.SetVertices (vertices);
+        mesh.SetTriangles (triangles, 0, true);
+        mesh.SetTriangles (sideTriangles, 1);
+        mesh.SetUVs (0, uvs);
+        mesh.SetNormals (normals);
+
+        if (mat != null && sideMat != null) {
+             meshRenderer.sharedMaterials = new Material[] { mat, sideMat};
+
+            Color[] startCols = { water.startCol, sand.startCol, grass.startCol };
+            Color[] endCols = { water.endCol, sand.endCol, grass.endCol };
+            mat.SetColorArray ("_StartCols", startCols);
+            mat.SetColorArray ("_EndCols", endCols);
+        }
+    }
+
+    [System.Serializable]
+    public class Biome {
+        [Range (0, 1)]
+        public float height;
+        public Color startCol;
+        public Color endCol;
+        public int numSteps;
+    }
+
+    void CreateMeshComponents () {
+        GameObject holder = null;
+
+        if (meshFilter == null) {
+            if (GameObject.Find (meshHolderName)) {
+                holder = GameObject.Find (meshHolderName);
+            } else {
+                holder = new GameObject (meshHolderName);
+                holder.AddComponent<MeshRenderer> ();
+                holder.AddComponent<MeshFilter> ();
+            }
+            meshFilter = holder.GetComponent<MeshFilter> ();
+            meshRenderer = holder.GetComponent<MeshRenderer> ();
+        }
+
+        if (meshFilter.sharedMesh == null) {
+            mesh = new Mesh ();
+            meshFilter.sharedMesh = mesh;
+        } else {
+            mesh = meshFilter.sharedMesh;
+            mesh.Clear ();
+        }
+
+        meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+    }
+
+    void OnValidate () {
+        needsUpdate = true;
+    }
+
+}
